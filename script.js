d7e0f06d92748f0971d22c668976940aceb389f8
Dmitr0 <dmitr062@mail.ru>
2024-08-08 19:09:55 +0300

    Complite rewiew-cards

--- script.js
+++ script.js
@@ -1,35 +1,11 @@
 $('.review-cards').slick({
-  infinite: false,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 990,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: true,
-        autoplay:true,
-        autoplaySpeed:5000,
-        arrows: false
-      } 
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        autoplay:true,
-        autoplaySpeed:5000,
-        arrows: false
-      }
-    }     
-  ]
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
 });
 $('.goods__card').hover(
   function() {
-    $('.goods__card').css('border', 'solid blue')
+    $('.goods__card').css('border', 'solid grey')
   }, function() {
     $('.goods__card').removeAttr('style')
   }
